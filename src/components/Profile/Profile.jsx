1bffc61ce524d86982d9805ce4a8e7d3781a6023
Kateryna Artemieva <moruga.katya@gmail.com>
2023-05-24 22:42:16 +0100

    task-1/css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -17,7 +17,7 @@ export const Profile = user => {
         <div className={css.description}>
           <img src={avatar} alt="User avatar" className={css.avatar} />
           <p className={css.name}>{username}</p>
-          <p className={css.descrText}>{tag}</p>
+          <p className={css.descrText}>@{tag}</p>
           <p className={css.descrText}>{location}</p>
         </div>
 
